da506e3d8072e1342c1a3728b04f6c9a25e3abd6
saeed123991 <saeedanwargee794@gmail.com>
2022-11-06 09:43:05 -0800

    Quiz 2
    Quiz 2 BMI Calculator

---
+++ bmi_calculator_app/README.md
@@ -0,0 +1,8 @@
+# bmi_calculator_app with flutter
+
+A new Flutter project.
+
+## Saeed Anwar (Sp18-BCS-071)
+
+
+
---
+++ bmi_calculator_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'Screens/input_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
